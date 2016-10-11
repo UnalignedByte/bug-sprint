88e435327de0fa99c81a1a429e035921300f859d
Rafal Grodzinski <rafal@EvilBook.local>
2016-10-11 09:05:47 +0100

    Cleanup

--- Bug Sprint/Source/AppDelegate.swift
+++ Bug Sprint/Source/AppDelegate.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate
 {
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool
+    {
         return true
     }
 }
---
+++ README.txt
@@ -0,0 +1,1 @@
+It's a game about running over bugs. It's written in C++ and works on iOS and Android.
