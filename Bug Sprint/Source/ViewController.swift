a3b02dbee2a72280b593f05a1fe4ba267f715f17
Rafal Grodzinski <rafal@EvilBook.local>
2016-10-11 09:00:35 +0100

    Initial commit

---
+++ Bug Sprint/Source/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bug Sprint
+//
+//  Created by Rafal Grodzinski on 11/10/2016.
+//  Copyright © 2016 UnalignedByte. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
+
