3e78dd3bf3b2eff7145a3cdcbe570906a0243882
Rafal Grodzinski <rafal@EvilBook.local>
2016-11-02 09:13:38 +0000

    Basic app loop

--- Bug Sprint/Source/Android Layer/Main.cpp
+++ Bug Sprint/Source/Android Layer/Main.cpp
@@ -1,7 +1,59 @@
 #include <android_native_app_glue.h>
 
 
+bool isSetup = false;
+
+void processAppCommands(android_app *app, int cmd);
+void setup();
+void executeLoop();
+
+
 void android_main(android_app *app)
 {
     app_dummy();
+
+    app->onAppCmd = processAppCommands;
+
+    int events;
+    android_poll_source *source;
+
+    while(true) {
+        while(ALooper_pollAll(0, NULL, &events, (void **) &source) >= 0) {
+            if(source != NULL)
+                source->process(app, source);
+
+            if(app->destroyRequested)
+                return;
+        }
+
+        executeLoop();
+    }
+}
+
+
+void processAppCommands(android_app *app, int cmd)
+{
+    switch(cmd) {
+        case APP_CMD_INIT_WINDOW:
+            setup();
+            break;
+        default:
+            break;
+    }
+}
+
+
+void setup()
+{
+    if(isSetup)
+        return;
+
+    isSetup = true;
+}
+
+
+void executeLoop()
+{
+    if(!isSetup)
+        return;
 }
